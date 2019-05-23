464fc552289e778a9aa8886b1bd617e9f87f098a
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-23 09:29:00 +0300

    rendered homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.Department;
+import models.Staff;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/",(request, response) -> {
+           Map<String,Object> model = new HashMap<String,Object>();
+           ArrayList<Staff> staff = Staff.getmInstances();
+           model.put("staff",staff);
+           return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
